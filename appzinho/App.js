82cacc52e498462db204542196052680d7d7ae29
GUILHERME SILVA SAMICO <202002324724@alunos.estacio.br>
2023-03-30 20:49:45 -0300

    add header with logo

--- appzinho/App.js
+++ appzinho/App.js
@@ -1,18 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.appTitle}>Bem vindo ao meu App</Text>
-      <Text style={styles.appText}>Desenvolvido por Guilherme</Text>
-      <Text style={styles.appText}>Em parceria com a estacio</Text>
+    <View style={styles.header}>
+      <Image style={styles.tinyLogo} source={
+        {uri: 'https://reactnative.dev/img/tiny_logo.png',}}/>
+      <View style={styles.container}>
+        <Text style={styles.appTitle}>Bem vindo ao meu App</Text>
+        <Text style={styles.appText}>Desenvolvido por Guilherme</Text>
+        <Text style={styles.appText}>Em parceria com a estacio</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  header: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 20,
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
@@ -21,12 +32,16 @@ const styles = StyleSheet.create({
   },
   appTitle: {
     fontSize: 30, 
-    color: 'red', 
+    color: 'black', 
     textAlign: 'center',
   },
   appText: {
     fontSize: 20, 
-    color: 'green', 
+    color: '#40918f', 
     textAlign: 'center',
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
   },
 });
